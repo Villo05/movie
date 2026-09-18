e388c2ffe36321365409ab632c49ff0ad87a3859
Villo05 <nemethvillo.2005@gmail.com>
2026-09-18 14:54:13 +0200

    seed

--- library/database/seeders/DatabaseSeeder.php
+++ library/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
+use App\Models\Copy;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,6 +18,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::factory(10)->create();
+        Book::factory(10)->create();
+        Copy::factory(5)->create();
+
 
         User::factory()->create([
             'name' => 'Test User',
